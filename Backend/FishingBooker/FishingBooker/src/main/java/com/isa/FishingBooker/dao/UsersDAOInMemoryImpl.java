8f38ec5cdb9a35f71bf842327a0c156787fe0e30
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-10-28 13:19:17 +0200

    feat(Backend): DAO interface and impl of UserInMemoryDao (addNew, getAll).

---
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/dao/UsersDAOInMemoryImpl.java
@@ -0,0 +1,44 @@
+package com.isa.FishingBooker.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.isa.FishingBooker.model.Status;
+import com.isa.FishingBooker.model.User;
+@Component("usersDAO")
+public class UsersDAOInMemoryImpl implements UsersDAO {
+	private List<User> users = new ArrayList<User>();
+
+	@Override
+	public List<User> getAll() {
+		return users;
+	}
+
+	@Override
+	public User getById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addNew(User item) {
+		users.add(item);
+	}
+
+}
