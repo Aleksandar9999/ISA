cecee6b39a4cc737fb83cb1658527d331e329fa6
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-10-28 11:43:28 +0200

    init projects

---
+++ Backend/FishingBooker/FishingBooker/src/test/java/com/isa/FishingBooker/FishingBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.FishingBooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
