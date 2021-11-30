830a72e195ea3d34bd1f927727822ed8ffa1392c
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-11-30 23:55:18 +0100

    feat(Backend): Jpql queries

--- Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/repository/TutorServiceRepository.java
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/repository/TutorServiceRepository.java
@@ -1,10 +1,19 @@
 package com.isa.FishingBooker.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.isa.FishingBooker.model.Tutor;
 import com.isa.FishingBooker.model.TutorService;
 @Repository
 public interface TutorServiceRepository extends JpaRepository<TutorService ,Integer> {
 
+	@Query("select tutorService from TutorService tutorService join fetch tutorService.photos photos where tutorService.id=?1")
+	public TutorService findTutorServiceWithPhotos(int tutorServiceId);
+	
+	@Query("select tutorService from TutorService tutorService join fetch tutorService.disconutOffers disconutOffers where tutorService.id=?1")
+	public TutorService findTutorServiceWithDiscountOffers(int tutorServiceId);
 }
