95abbe557c8bd04baa780a0838a17d6fb07e8df0
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-12-01 21:26:05 +0100

    feat(Backend): GET/CREATE delete profile request

---
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/mapper/DeleteRequestMapper.java
@@ -0,0 +1,26 @@
+package com.isa.FishingBooker.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.isa.FishingBooker.dto.DeleteRequestDTO;
+import com.isa.FishingBooker.model.DeleteRequest;
+import com.isa.FishingBooker.model.User;
+
+@Component("deleteRequestMapper")
+public class DeleteRequestMapper extends CustomModelMapperAbstract<DeleteRequest, DeleteRequestDTO> {
+
+	@Override
+	public DeleteRequestDTO convertToDto(DeleteRequest entity) {
+		DeleteRequestDTO req=modelMapper.map(entity, DeleteRequestDTO.class);
+		req.setUserId(entity.getId());
+		return req;
+	}
+
+	@Override
+	public DeleteRequest convertToEntity(DeleteRequestDTO dto) {
+		DeleteRequest req=modelMapper.map(dto, DeleteRequest.class);
+		req.setUser(new User(dto.getUserId()));
+		return req;
+	}
+
+}
---
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/repository/DeleteRequestRepository.java
@@ -0,0 +1,9 @@
+package com.isa.FishingBooker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.isa.FishingBooker.model.DeleteRequest;
+
+public interface DeleteRequestRepository extends JpaRepository<DeleteRequest, Integer> {
+
+}
