8df5673b01d6f3adb82da9af99d3f4e96a331a01
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-12-01 16:55:09 +0100

    feat(BACKEND): GET/CREATE Appointment for tutor service

--- Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/controller/AppointmentController.java
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/controller/AppointmentController.java
@@ -6,12 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.isa.FishingBooker.model.Appoinment;
+import com.isa.FishingBooker.dto.TutorServiceAppointmentDTO;
+import com.isa.FishingBooker.mapper.CustomModelMapper;
+import com.isa.FishingBooker.mapper.CustomModelMapperAbstract;
+import com.isa.FishingBooker.model.Appointment;
 import com.isa.FishingBooker.model.BoatAppointment;
 import com.isa.FishingBooker.model.ResortAppointment;
+import com.isa.FishingBooker.model.Tutor;
+import com.isa.FishingBooker.model.TutorService;
 import com.isa.FishingBooker.model.TutorServiceAppointment;
+import com.isa.FishingBooker.model.User;
 import com.isa.FishingBooker.service.AppointmentService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -20,26 +29,43 @@ public class AppointmentController {
 
 	@Autowired
 	private AppointmentService service;
-	
+	@Autowired
+	private CustomModelMapper<TutorServiceAppointment, TutorServiceAppointmentDTO> tutorServiceModelMapper;
+
 	@GetMapping("/appointments")
-	public ResponseEntity<ArrayList<Appoinment>>  getAll(){
-		return ResponseEntity.ok((ArrayList<Appoinment>)service.getAll());
+	public ResponseEntity<ArrayList<Appointment>> getAll() {
+		return ResponseEntity.ok((ArrayList<Appointment>) service.getAll());
 	}
-	
+
 	@GetMapping("/resortAppointments")
-	public ResponseEntity<ArrayList<ResortAppointment>>  getAllResort(){
-		return ResponseEntity.ok((ArrayList<ResortAppointment>)service.getResortApointments());
+	public ResponseEntity<ArrayList<ResortAppointment>> getAllResort() {
+		return ResponseEntity.ok((ArrayList<ResortAppointment>) service.getResortApointments());
 	}
-	
+
 	@GetMapping("/boatAppointments")
-	public ResponseEntity<ArrayList<BoatAppointment>>  getAllBoat(){
-		return ResponseEntity.ok((ArrayList<BoatAppointment>)service.getBoatApointments());
+	public ResponseEntity<ArrayList<BoatAppointment>> getAllBoat() {
+		return ResponseEntity.ok((ArrayList<BoatAppointment>) service.getBoatApointments());
 	}
-	
-	@GetMapping("/tutorServiceAppointments")
-	public ResponseEntity<ArrayList<TutorServiceAppointment>>  getAllTutorService(){
-		return ResponseEntity.ok((ArrayList<TutorServiceAppointment>)service.getTutorServiceApointments());
+
+	@GetMapping("api/tutor-service/appointments")
+	public ResponseEntity<ArrayList<TutorServiceAppointment>> getAllTutorServiceAppointments() {
+		return ResponseEntity.ok((ArrayList<TutorServiceAppointment>) service.getTutorServiceApointments());
 	}
-	
-	
+
+	@PostMapping("api/appointments/tutor-service/{idservice}")
+	public ResponseEntity addTutorServiceAppointment(@RequestBody TutorServiceAppointmentDTO dto,
+			@PathVariable("idservice") Integer idService) {
+		TutorServiceAppointment appointment= tutorServiceModelMapper.convertToEntity(dto);
+		appointment.setTutorService(new TutorService(idService));
+		appointment.setUser(new User(1)); // TODO: Get id of loggedin user
+		service.addNew(appointment);
+		return ResponseEntity.ok(tutorServiceModelMapper.convertToDto(appointment));
+	}
+
+	@GetMapping("api/users/tutors/{idtutor}/tutor-service/appointments")
+	public ResponseEntity getAllAppointmentsByTutor(@RequestBody TutorServiceAppointment appointment,
+			@PathVariable("idtutor") Integer idtutor) {
+		return ResponseEntity.ok(tutorServiceModelMapper.convertToDtos(service.getAllTutorServiceAppointmentsByTutor(idtutor)));
+	}
+
 }
