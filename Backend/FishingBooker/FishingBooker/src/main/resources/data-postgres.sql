3870e8c860de147751ad66311b544e5955f2f5c4
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-12-02 18:32:51 +0100

    fix(Backend): Fixed name of table in sql

--- Backend/FishingBooker/FishingBooker/src/main/resources/data-postgres.sql
+++ Backend/FishingBooker/FishingBooker/src/main/resources/data-postgres.sql
@@ -36,7 +36,7 @@ insert into period (start_date, end_date) values('Mon Dec 13 10:00:00 GMT 2021',
 insert into period (start_date, end_date) values('Mon Dec 20 10:00:00 GMT 2021','Sat Dec 25 15:00:00 GMT 2021');
 insert into period (start_date, end_date) values('Mon Dec 27 10:00:00 GMT 2021','Sat Jan 01 15:00:00 GMT 2021');
 
-insert into appoinment (dtype, start, duration, max_person, additional_services, price, address_id, user_id, resort_id) values ('ResortAppointment', 'Sat Nov 27 18:04:41 GMT 2021',3.0, 7, 'No additional services.', 500.00, 4, 1, 1);
+insert into appointment (dtype, start, duration, max_person, additional_services, price, address_id, user_id, resort_id) values ('ResortAppointment', 'Sat Nov 27 18:04:41 GMT 2021',3.0, 7, 'No additional services.', 500.00, 4, 1, 1);
 
 insert into discount_offer (additional_services, max_person, place, price, boat_id, reservation_per_id, validity_per_id) values ('None',7, 'Amajic, Zapadna Srbija', 1400.0, 2, 1, 5);
 insert into discount_offer (additional_services, max_person, place, price, boat_id, reservation_per_id, validity_per_id) values ('None',7, 'Vrhpolje, Zapadna Srbija', 1200.0, 1, 2, 6);
