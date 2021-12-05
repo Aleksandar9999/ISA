8e072ef1b986bc0b8cfd6484092282f5ff311017
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-12-05 16:45:53 +0100

    feat(Frontend): Tutor creating new appointment

--- Frontend/fishing-booker-app/src/components/TutorPanel/TutorServices/Appointments/AppointmentsList.vue
+++ Frontend/fishing-booker-app/src/components/TutorPanel/TutorServices/Appointments/AppointmentsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="padding-top: 5%">
     <w-flex justify-end class="pa3" style="padding: 0% 20%">
-      <w-button @click="addNewPeriod">Add new</w-button>
+      <w-button @click="dialog.show=true">Add new</w-button>
     </w-flex>
     <CustomTable
       :dataList="data"
@@ -9,22 +9,29 @@
       :itemRow="itemRow"
       @showUserProfile="showUserDialog"
     />
+    <NewAppointmentModalDialog :idTutor=dialog.id :show=dialog.show @hideDialog="hideModalDialog" />
   </div>
 </template>
 <script>
 import axios from "axios";
 import CustomTable from "../../../CustomTable.vue";
 import AppointmentInfoRow from "./AppointmentInfoRow.vue";
+import NewAppointmentModalDialog from "./NewAppointmentModalDialog.vue"
 export default {
   components: {
     CustomTable,
     AppointmentInfoRow,
+    NewAppointmentModalDialog
   },
   data() {
     return {
       data: [],
       headers: ["START DATE", "DURATION", "NUMBER OF PERSONS", "PRICE", ""],
       itemRow: AppointmentInfoRow,
+      dialog:{
+        show: false,
+        id:4
+      }
     };
   },
   mounted() {
@@ -46,6 +53,10 @@ export default {
       console.log(event);
     },
     addNewPeriod() {},
+    hideModalDialog(event){
+      this.dialog.show=event.dialog
+      console.log(event)
+    }
   },
 };
 </script>
