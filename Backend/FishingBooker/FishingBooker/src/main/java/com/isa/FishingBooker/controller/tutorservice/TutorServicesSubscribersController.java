10a0df568d0e9e121ab1046c948fb5e20abb5f47
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-12-21 10:19:41 +0100

    feat(Backend): Add new subscriber for tutor service

---
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/controller/tutorservice/TutorServicesSubscribersController.java
@@ -0,0 +1,21 @@
+package com.isa.FishingBooker.controller.tutorservice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TutorServicesSubscribersController extends TutorServicesAbstractController {
+	@GetMapping("api/tutor-services/{id}/subscribers")
+	public ResponseEntity<?> getAllSubscribers(@PathVariable("id")int id){
+		return ResponseEntity.ok(this.tutorServicesService.getById(id).getSubscribers());
+	}
+	
+	@PostMapping("api/tutor-services/{id}/subscribers")
+	public ResponseEntity<?> addNewSubscriber(@PathVariable("id")int serviceId){
+		tutorServicesService.addNewSubscriber(serviceId,this.getLoggedinUser());
+		return ResponseEntity.ok().build();
+	}
+}
