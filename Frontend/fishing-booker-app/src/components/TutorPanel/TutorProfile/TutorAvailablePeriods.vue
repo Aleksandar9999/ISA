8a437ed4b9c1567862d910d47ee2cac20b07132a
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2021-12-21 05:55:36 +0100

    feat(Frontend): Add new standard period for selected tutor service

--- Frontend/fishing-booker-app/src/components/TutorPanel/TutorProfile/TutorAvailablePeriods.vue
+++ Frontend/fishing-booker-app/src/components/TutorPanel/TutorProfile/TutorAvailablePeriods.vue
@@ -2,14 +2,42 @@
   <div style="padding-top: 5%">
     <w-flex justify-end class="pa3" style="padding: 0% 20%">
       <w-input type="date" v-model="newPeriod.startDate"> </w-input>
+      <w-input
+        type="text"
+        placeholder="12:00"
+        v-model="newPeriod.startTime"
+      ></w-input>
       <w-input type="date" v-model="newPeriod.endDate"> </w-input>
+      <w-input
+        type="text"
+        placeholder="12:00"
+        v-model="newPeriod.endTime"
+      ></w-input>
+      <w-flex wrap class="text-right">
+        <div class="xs6 pa1">
+          <select
+            name="services"
+            id="services"
+            v-model="selectedService"
+            @change="selectedServiceChange"
+          >
+            <option value="">Select service</option>
+            <option
+              v-for="service in services"
+              :key="service.id"
+              :value="service.id"
+            >
+              {{ service.name }}
+            </option>
+          </select>
+        </div>
+      </w-flex>
       <w-button @click="addNewPeriod">Add new</w-button>
     </w-flex>
     <CustomTable :dataList="data" :headerList="headers" :itemRow="itemRow" />
   </div>
 </template>
 <script>
-import axios from "axios";
 import CustomTable from "../../CustomTable.vue";
 import AvailablePeriodInfoRow from "./AvailablePeriodInfoRow.vue";
 import config from "../../../configuration/config";
@@ -23,36 +51,71 @@ export default {
       data: [],
       headers: ["START DATE", "END DATE", ""],
       itemRow: AvailablePeriodInfoRow,
+      services: [],
       newPeriod: {
         startDate: "",
+        startTime: "",
         endDate: "",
+        endTime: "",
       },
+      selectedService: "",
+      idTutor: this.$route.params.idtutor,
+      api: `${config.apiStart}/api/users/tutors/${this.$route.params.idtutor}`,
     };
   },
   mounted() {
+    console.log("ROUTE ID TUTOR:" + this.$route.params.idtutor);
     this.fetchData();
   },
   methods: {
+    fetchStandardPeriodsForAllTutorServices() {
+      this.$axios.get(`${this.api}/standard-periods`).then((resp) => {
+        this.data = resp.data;
+        console.log(this.data);
+      });
+    },
+    fetchTutorServices() {
+      this.$axios
+        .get(`${this.api}/services`)
+        .then((resp) => (this.services = resp.data));
+    },
     fetchData() {
-      axios
-        .get(config.apiStart + "/api/users/tutors/4/available-periods",config.requestHeader)
+      this.fetchStandardPeriodsForAllTutorServices();
+      this.fetchTutorServices();
+    },
+    selectedServiceChange(event) {
+      let value = event.target.value;
+      if (!value) {
+        this.fetchStandardPeriodsForAllTutorServices();
+        return;
+      }
+      this.$axios
+        .get(`${config.apiStart}/api/tutor-services/${value}/standard-periods`)
         .then((resp) => {
           this.data = resp.data;
           console.log(this.data);
         });
     },
     addNewPeriod() {
-      console.log(this.newPeriod);
-      axios
+      console.log({
+        startDate: `${this.newPeriod.startDate} ${this.newPeriod.startTime}`,
+        endDate: `${this.newPeriod.endDate} ${this.newPeriod.endTime}`,
+      });
+      this.$axios
         .post(
-          config.apiStart + "/api/users/tutors/4/available-periods",
-          this.newPeriod,config.requestHeader
+          `${config.apiStart}/api/tutor-services/${this.selectedService}/standard-periods`,
+          {
+            startDate: `${this.newPeriod.startDate} ${this.newPeriod.startTime}:00`,
+            endDate: `${this.newPeriod.endDate} ${this.newPeriod.endTime}:00`,
+          }
         )
-        .then((resp) => {
-          console.log(resp);
+        .then(() => {
           this.newPeriod.startDate = "";
           this.newPeriod.endDate = "";
-          this.fetchData()
+          this.newPeriod.startTime = "";
+          this.newPeriod.endTime = "";
+          this.selectedService = "";
+          this.fetchData();
         });
     },
   },
