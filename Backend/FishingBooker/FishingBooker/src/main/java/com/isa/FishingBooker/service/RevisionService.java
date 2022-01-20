2582e53777dc2760851852cc10045fdc4c8cdbe4
Aleksandar Brkljac <aleksandar.brkljac@vega.local>
2022-01-20 12:40:05 +0100

    fix(Backend): Fixed api to get rate for service and refactor implementation

--- Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/service/RevisionService.java
+++ Backend/FishingBooker/FishingBooker/src/main/java/com/isa/FishingBooker/service/RevisionService.java
@@ -10,7 +10,7 @@ public interface RevisionService extends Service<Revision> {
 	
 	public int getBoatAppointmentRevisionsRate(Integer id);
 	public int getResortAppointmentRevisionsRate(Integer id);
-	public int getTutorServiceAppointmentRevisionsRate(Integer id);
+	public double getTutorServiceAppointmentRevisionsRate(Integer id);
 	public String makeTutorServiceRevision(TutorServiceAppointmentRevision revision);
 	public String makeBoatRevision(BoatAppointmentRevision revision);
 	public String makeResortRevision(ResortAppointmentRevision revision);
