3e7f9b5d58f682d559da54828e00771343f32aa1
ra149 <fresh.trifunovic@gmail.com>
2022-06-02 19:02:23 +0200

    feat(frontend): proba chartova

--- Frontend/fishing-booker-app/src/components/BoatsPanel/Boats/Chart.vue
+++ Frontend/fishing-booker-app/src/components/BoatsPanel/Boats/Chart.vue
@@ -1,22 +1,154 @@
 <template>
-    <div>
-        <canvas id="myChart" width="400" height="400"></canvas>
+    <div  >
+    <div
+      class="card mx-auto"
+      style="
+        width: 400px;
+        margin-top: 20px;
+        margin-right: 20px;
+        margin-left: 40px;
+        margin-bottom: 20px;
+      "
+    >
+      <line-chart :data="dataChart" :options="{responsive: true, maintainAspectRatio: false}"></line-chart>
+      <canvas id="appChart"></canvas>
+      <div>
+        <button @click="appChartYearly()" mat-button color="primary">
+          Yearly
+        </button>
+        <button @click="appChartMonthly()" mat-button color="primary">
+          Monthly
+        </button>
+        <button @click="appChartWeekly()" mat-button color="primary">
+          Weekly
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 
 import Chart from 'chart.js/auto';
+
+
+
 export default{
-    mounted(){
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
+   components: { 
+     
+   },
+   data() {
+    return {
+      boatOwnerId: this.$route.params.idboatowner,
+      appChart: Chart,
+      myChart: Chart,
+      currentTime: new Date(),
+      yearLabels: [
+    ((new Date()).getFullYear()- 2).toString(),
+    ((new Date()).getFullYear() - 1).toString(),
+    ((new Date()).getFullYear()).toString(),
+    ((new Date()).getFullYear() + 1).toString(),
+      ],
+
+    monthsLabels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+    daysLabels:  [],
+    labels:  [],
+  yearAppSum: [0, 0, 0],
+  monthsAppSum: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  daysAppSum: [0, 0, 0, 0],
+  data5: [
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170,
+    180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310,
+  ],
+  data1: [
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170,
+    180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310,
+  ],
+  data2:[
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170,
+    180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310,
+  ],
+   };
+  },
+  methods: {
+    loadChartAppointment(){
+  },
+   addDaysLabels(){
+    let labelsDays = [];
+    var month = this.currentTime.getMonth();
+    var d = new Date(this.currentTime.getFullYear(), month, 0);
+    var lastDay = d.getDate();
+    console.log(lastDay.toString());
+    for (let i = 1; i <= 3; i++) {
+      labelsDays.push(String(i * 7));
+      console.log(i);
+    }
+    labelsDays.push(String(lastDay));
+    return labelsDays;
+  },
+  appChartYearly() {
+      console.log(this.myChart.data.datasets[0]);
+    //this.myChart.data.datasets[0].data = [1,2];
+  
+    this.myChart.data.datasets[0].labels = ['1','2'];
+    
+    // this._doctorStatisticsService
+    //   .getAppointmentsCountYearly(this.doctorId)
+    //   .subscribe((data) => {
+    //     this.appChart.data.datasets[0].data = data.yearlySum;
+    //     this.appChart.update();
+    //   });
+    // this.appChart.config.type = 'pie' as ChartType;
+    this.myChart.update();
+  },
+  appChartMonthly() {
+    this.myChart.data.labels = this.monthsLabels;
+    // this.appChart.config.type = 'bar' as ChartType;
+    // this._doctorStatisticsService
+    //   .getAppointmentsCountMonthly(this.doctorId)
+    //   .subscribe((data) => {
+    //     this.appChart.data.datasets[0].data = data.monthlySum;
+    //     this.appChart.update();
+    //   });
+    this.myChart.update();
+  },
+    appChartWeekly() {
+    this.myChart.data.labels = this.daysLabels;
+    // this.appChart.config.type = 'bar' as ChartType;
+    // this._doctorStatisticsService
+    //   .getAppointmentsCountWeekly(this.doctorId)
+    //   .subscribe((data) => {
+    //     this.appChart.data.datasets[0].data = data.weeklySum;
+    //     this.appChart.update();
+    //   });
+    this.myChart.update();
+  },
+  
+
+  },
+  
+  mounted(){
+var appChartItem = document.getElementById('appChart').getContext('2d');
+ this.myChart = new Chart(appChartItem, {
     type: 'bar',
     data: {
         labels: ['Januar', 'Februar', 'Mart', 'April', 'Maj', 'Jun','Jul','Septembar','Oktobar','Novembar','Decembar'],
         datasets: [{
             label: '# of Votes',
-            data: [30, 0, 3, 5, 2, 3,10,20,15,11,25,35],
+            data: this.data5,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -44,8 +176,19 @@ const myChart = new Chart(ctx, {
         }
     }
   });
-  myChart;
+  this.myChart;
+  // this.appChartItem = document.getElementById('appChart');
+   //  this.loadChartAppointment();
+     console.log(this.data5);
+      console.log(this.yearLabels);
+
+     this.daysLabels = this.addDaysLabels();
+     this.myChart.data.labels = this.monthsLabels;
+     this.myChart.update();
+     console.log(this.daysLabels);
+     console.log(this.myChart.data);
     }
+  
 }
 
 
